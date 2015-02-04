371f5b6cba3889cc960a267bd75f263884ad3f45
Lars Grefer <larsgrefer@freenet.de>
2015-02-04 21:44:59 +0100

    first commit

---
+++ app/src/main/java/de/larsgrefer/android/library/helper/FloatLabelActivity.java
@@ -0,0 +1,19 @@
+package de.larsgrefer.android.library.helper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.larsgrefer.android.library.injection.annotation.XmlLayout;
+import de.larsgrefer.android.library.injection.annotation.XmlView;
+import de.larsgrefer.android.library.ui.injection.InjectionActivity;
+
+/**
+ * Created by larsgrefer on 31.01.15.
+ */
+@XmlLayout(R.layout.activity_float_label)
+public class FloatLabelActivity extends InjectionActivity {
+
+
+}
